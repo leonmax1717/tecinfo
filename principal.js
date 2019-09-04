44b397e9f16a6b8b53d1947510d90f07ac3a62c3
Leon Max <leonmax0120@gmail.com>
2019-09-04 19:38:07 -0300

    Inserindo e diminuindo tamanho da pagina

---
+++ principal.js
@@ -0,0 +1,11 @@
+var banners =
+	["Os melhores do brasil", "Qualidade e preço baixo!"];
+var bannersAtual = 0;
+
+function trocaBanner() {
+	bannersAtual = (bannersAtual + 1) % 2;
+	document.querySelector('h2mensagem').textContent = 
+	banners[bannersAtual];
+}
+
+setInterval(trocaBanner, 2000);
